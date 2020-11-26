609b17a990435affdd29f3d04de45e8aee036feb
linchuaccount <younchu123@gmail.com>
2020-11-26 23:23:04 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
