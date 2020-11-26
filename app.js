f612f5766e0e793a8504ff8b308fb907bcb65f97
linchuaccount <younchu123@gmail.com>
2020-11-27 00:01:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mogodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('test')
