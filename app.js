d178ee5bf8a59c6e0bc32eafb964c50b0286f6d2
linchuaccount <younchu123@gmail.com>
2020-11-27 15:51:12 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,6 +17,7 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //載入Restaurant models
 app.get('/', (req, res) => {
@@ -23,7 +25,20 @@ app.get('/', (req, res) => {
   .lean()
   .then( restaurants => res.render('index', { restaurants }))
   .catch(error => console.error(error))
-  
+})
+
+//載入新增頁面new
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+//接住新增頁面的資料，並送進db儲存
+app.post('/restaurants', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body 
+  //解構賦值:可以把物件裡的屬性一項項拿出來存成變數
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })     
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 
