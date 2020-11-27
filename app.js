3f19c4326989e22cb8510ab69b73466f7e285d24
linchuaccount <younchu123@gmail.com>
2020-11-27 21:53:23 +0800

    feat: add stylesheets(index.css)& modify main.hbs

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 //載入Restaurant models
 app.get('/', (req, res) => {
