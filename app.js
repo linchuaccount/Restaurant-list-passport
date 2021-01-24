da32c965305d17cbe3888e435bba99523d45bfba
linchuaccount <younchu123@gmail.com>
2021-01-24 17:29:25 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -11,6 +11,14 @@ const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'PersonalRestaurantList',
+  resave: false,
+  saveUninitialized: true
+
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
