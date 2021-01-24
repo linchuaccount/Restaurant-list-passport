b80f8c122b9217959c828aedeb6285e8b1fe2b89
linchuaccount <younchu123@gmail.com>
2021-01-24 15:25:52 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./modules/home')// 引入 home 模組程式碼
-router.use('/', home)// 將網址結構符合 / 字串的 request 導向 home 模組 
-
+// 引入 各個路由 的 模組程式碼
+const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-router.use('/restaurants', restaurants)
-
 const search = require('./modules/search')
-router.use('/search', search)
-
 const sort = require('./modules/sort')
+const users = require('./modules/users')
+
+// 將網址結構符合 / 字串的 request 導向 對應的模組程式碼 
+router.use('/', home)
+router.use('/restaurants', restaurants)
+router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
+
 
 // 匯出路由器
 module.exports = router
