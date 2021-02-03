0ba39bcfcd59ace58e524d92a7aef56a777bd437
linchuaccount <younchu123@gmail.com>
2021-02-03 14:18:48 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,24 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   //初始化Paspport模組
   app.use(passport.initialize())
   app.use(passport.session())
   //設定LocalStrategy
-  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, ( req, email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
-      .then(user =>{
-        if(!user) {
+      .then(user => {
+        if (!user) {
           return done(null, false, req.flash('warning_msg', 'email尚未註冊 , 請前往註冊。'))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('warning_msg', '帳號或密碼輸入錯誤。'))
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) { return done(null, false, req.flash('warning_msg', '帳號或密碼輸入錯誤。')) }
+            return done(null, user)
+          })
       })
       .catch(err => done(err, false))
   }))
@@ -26,8 +28,8 @@ module.exports = app => {
   })
   passport.deserializeUser((id, done) => {
     User.findById(id)
-    .lean()
-    .then(user => done(null, user))
-    .catch(err => done(err, null))
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
   })
 }
